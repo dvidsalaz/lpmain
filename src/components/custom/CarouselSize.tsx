e943327cb1ebe88e61d391d8e0df794363af2077
David <dvidsalaz@gmail.com>
2025-01-09 16:46:54 -0600

    added more services

--- src/components/custom/CarouselSize.tsx
+++ src/components/custom/CarouselSize.tsx
@@ -35,7 +35,7 @@ export function CarouselSize() {
               className="flex flex-col items-center justify-center text-center gap-5 p-3 sm:p-5 lg:p-10 h-[350px]"
             >
               <h3 className=" font-oswald font-bold text-xl sm:text-2xl lg:text-3xl text-WayneBlack underline">
-                Roof Storm Damage Repair
+                Roof Leak Repair
               </h3>
               <p className=" flex-1 sm:text-lg lg:text-xl">
                 Filing a claim is simple and takes only a few minutes. We guide
@@ -67,7 +67,7 @@ export function CarouselSize() {
               className="flex flex-col items-center justify-center text-center gap-5 p-3 sm:p-5 lg:p-10 h-[350px]"
             >
               <h3 className=" font-oswald font-bold text-xl sm:text-2xl lg:text-3xl text-WayneBlack underline">
-                Full Roof Replacement
+                Shingle Replacement
               </h3>
               <p className=" flex-1 sm:text-lg lg:text-xl">
                 Filing a claim is simple and takes only a few minutes. We guide
@@ -99,7 +99,7 @@ export function CarouselSize() {
               className="flex flex-col items-center justify-center text-center gap-5 p-3 sm:p-5 lg:p-10 h-[350px]"
             >
               <h3 className="font-oswald font-bold text-xl sm:text-2xl lg:text-3xl text-WayneBlack underline">
-                Siding & Fascia Repair
+                Storm Damage Repair
               </h3>
               <p className=" flex-1 sm:text-lg lg:text-xl">
                 Filing a claim is simple and takes only a few minutes. We guide
@@ -131,7 +131,39 @@ export function CarouselSize() {
               className="flex flex-col items-center justify-center text-center gap-5 p-3 sm:p-5 lg:p-10 h-[350px]"
             >
               <h3 className="font-oswald font-bold text-xl sm:text-2xl lg:text-3xl text-WayneBlack underline">
-                Gutter Installation
+                Roof Replacement
+              </h3>
+              <p className=" flex-1 sm:text-lg lg:text-xl">
+                Filing a claim is simple and takes only a few minutes. We guide
+                you through the process, ensuring the insurance company is
+                informed of our collaboration and your claim details.
+              </p>
+              <Link href="/services">
+                <Button className="text-base sm:text-lg lg:text-xl gap-2 text-white p-6 bg-SunsetOrange rounded-none">
+                  LEARN MORE
+                </Button>
+              </Link>
+            </div>
+          </Card>
+        </CarouselItem>
+        <CarouselItem className="md:basis-1/2 lg:basis-1/3 ">
+          <Card className="flex flex-col bg-cloudyGray flex-1 rounded-none border-none">
+            <div className=" relative w-full h-96 overflow-hidden">
+              <Image
+                src="/images/insurance.jpg"
+                alt="A flexible image"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <Separator className=" bg-SunsetOrange p-1"></Separator>
+
+            <div
+              id="text-space"
+              className="flex flex-col items-center justify-center text-center gap-5 p-3 sm:p-5 lg:p-10 h-[350px]"
+            >
+              <h3 className="font-oswald font-bold text-xl sm:text-2xl lg:text-3xl text-WayneBlack underline">
+                Gutter Repair & Installation
               </h3>
               <p className=" flex-1 sm:text-lg lg:text-xl">
                 Filing a claim is simple and takes only a few minutes. We guide
