09dc2fa665788535da9c7da14b54fb6cdff8f9a5
David S <dvidsalaz@gmail.com>
2025-01-18 22:42:09 -0600

    added skeleton for services

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -56,10 +56,10 @@ export default function Team() {
       <section className="flex flex-col h-screen bg-slate-400">
         <div className="flex-grow flex flex-col gap-20 p-6 sm:p-10 lg:p-20">
           <MyNavigationBar></MyNavigationBar>
-          <h1 className="text-6xl sm:text-8xl lg:text-9xl text-white font-bold">
+          <h1 className="text-6xl sm:text-8xl lg:text-9xl text-white font-bold motion-opacity-in-0 motion-translate-y-in-100 motion-delay-100">
             SERVICES
           </h1>
-          <h3 className=" text-lg sm:text-xl lg:text-2xl font-light text-white">
+          <h3 className=" text-lg sm:text-xl lg:text-2xl font-light text-white motion-opacity-in-0 motion-translate-y-in-100 motion-delay-500">
             SETTING THE NEW STANDARD FOR ROOFING
           </h3>
         </div>
@@ -107,7 +107,7 @@ export default function Team() {
         >
           <div>
             <h2 className=" text-gray-300 text-2xl sm:text-3xl lg:text-5xl">
-              About Us
+              Storm Damage
             </h2>
             <h1 className="text-2xl text-white sm:text-3xl lg:text-5xl">
               We work together to make great things happen.
@@ -116,6 +116,12 @@ export default function Team() {
           <Link href="/about">
             <Button className=" text-base sm:text-lg lg:text-xl gap-2 text-white p-6 bg-SunsetOrange rounded-none">
               LEARN MORE
+              <Image
+                src="/images/arrow-up-right.svg"
+                alt="arrow"
+                width={24}
+                height={24}
+              />
             </Button>
           </Link>
         </div>
@@ -126,7 +132,7 @@ export default function Team() {
         >
           <div>
             <h2 className="text-gray-300 text-2xl sm:text-3xl lg:text-5xl">
-              Our Team
+              Roof Leaks
             </h2>
             <h1 className="text-2xl text-white sm:text-3xl lg:text-5xl">
               Impact-driven projects that build Houston for better.
@@ -135,61 +141,100 @@ export default function Team() {
           <div>
             <Link href="/team">
               <Button className=" text-base sm:text-lg lg:text-xl gap-2 text-white p-6 bg-SunsetOrange rounded-none">
-                MEET OUR TEAM
+                LEARN MORE
+                <Image
+                  src="/images/arrow-up-right.svg"
+                  alt="arrow"
+                  width={24}
+                  height={24}
+                />
               </Button>
             </Link>
           </div>
+        </div>
+      </section>
+      <section id="team" className=" flex flex-col md:flex-row h-screen">
+        <div
+          id="div1"
+          className=" shadow-inner-before hover:shadow-inner-after bg-cover bg-center bg-purple-500 justify-end flex-1 gap-8 flex flex-col p-6 sm:p-10 lg:p-20 items-start text-left"
+        >
+          <div>
+            <h2 className=" text-gray-300 text-2xl sm:text-3xl lg:text-5xl">
+              Shingle Replacement
+            </h2>
+            <h1 className="text-2xl text-white sm:text-3xl lg:text-5xl">
+              We work together to make great things happen.
+            </h1>
+          </div>
+          <Link href="/about">
+            <Button className=" text-base sm:text-lg lg:text-xl gap-2 text-white p-6 bg-SunsetOrange rounded-none">
+              LEARN MORE{" "}
+              <Image
+                src="/images/arrow-up-right.svg"
+                alt="arrow"
+                width={24}
+                height={24}
+              />
+            </Button>
+          </Link>
+        </div>
+        <div
+          id="div2"
+          className="shadow-inner-before hover:shadow-inner-after bg-purple-500 bg-center bg-cover justify-end flex-1 gap-8 flex flex-col p-6 sm:p-10 lg:p-20 items-start text-left"
+          style={{ backgroundImage: "url('/images/IMG_7163.jpg')" }}
+        >
+          <div>
+            <h2 className="text-gray-300 text-2xl sm:text-3xl lg:text-5xl">
+              Roof Replacement
+            </h2>
+            <h1 className="text-2xl text-white sm:text-3xl lg:text-5xl">
+              Impact-driven projects that build Houston for better.
+            </h1>
+          </div>
+          <div>
+            <Link href="/team">
+              <Button className=" text-base sm:text-lg lg:text-xl gap-2 text-white p-6 bg-SunsetOrange rounded-none">
+                LEARN MORE
+                <Image
+                  src="/images/arrow-up-right.svg"
+                  alt="arrow"
+                  width={24}
+                  height={24}
+                />
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section id="team" className=" flex flex-col md:flex-row h-screen">
+        <div
+          id="div1"
+          className=" shadow-inner-before hover:shadow-inner-after bg-cover bg-center justify-end flex-1 gap-8 flex flex-col p-6 sm:p-10 lg:p-20 items-start text-left"
+          style={{ backgroundImage: "url('/images/IMG_7163.jpg')" }}
+        >
+          <div>
+            <h2 className=" text-gray-300 text-2xl sm:text-3xl lg:text-5xl">
+              Gutter Repair & Installment
+            </h2>
+            <h1 className="text-2xl text-white sm:text-3xl lg:text-5xl">
+              We work together to make great things happen.
+            </h1>
+          </div>
+          <Link href="/about">
+            <Button className=" text-base sm:text-lg lg:text-xl gap-2 text-white p-6 bg-SunsetOrange rounded-none">
+              LEARN MORE{" "}
+              <Image
+                src="/images/arrow-up-right.svg"
+                alt="arrow"
+                width={24}
+                height={24}
+              />
+            </Button>
+          </Link>
         </div>
       </section>
       <Separator className=" bg-SunsetOrange p-1"></Separator>
-      <section
-        id="member"
-        className=" flex flex-col md:flex-row gap-8 p-6 sm:p-10 lg:p-20 bg-cloudyGray"
-      >
-        <div
-          id="div1"
-          className=" flex-1 gap-8 flex flex-col p-6 sm:p-10 lg:p-20 bg-white shadow items-center text-center md:text-left md:items-start"
-        >
-          <div>
-            <h2 className=" text-WayneBlack text-2xl sm:text-3xl lg:text-5xl">
-              Joaquin Salazar
-            </h2>
-            <h1 className="text-2xl text-gray-300 sm:text-3xl lg:text-5xl">
-              Founder
-            </h1>
-          </div>
-          <p className=" text-base sm:text-lg lg:text-xl">
-            Egestas fringilla aliquam leo, habitasse arcu varius lorem elit.
-            Neque pellentesque donec et tellus ac varius tortor, bibendum. Nulla
-            felis ac turpis at amet. Purus malesuada placerat arcu at enim elit
-            in accumsan.
-          </p>
-          <p className=" text-base sm:text-lg lg:text-xl">
-            Egestas fringilla aliquam leo, habitasse arcu varius lorem elit.
-            Neque pellentesque donec et tellus ac varius tortor, bibendum. Nulla
-            felis ac turpis at amet. Purus malesuada placerat arcu at enim elit
-            in accumsan.
-          </p>
-          <div>
-            <Link href="/team">
-              <Button className=" text-base sm:text-lg lg:text-xl gap-2 text-white p-6 bg-slate-400 rounded-none">
-                CONTACT
-              </Button>
-            </Link>
-          </div>
-        </div>
-        <div className="relative flex-1 flex items-center justify-center md:flex-col">
-          <Image
-            src="/images/IMG_1684.jpg"
-            alt="founderpicture"
-            layout="responsive"
-            width={500}
-            height={500}
-            objectFit=" cover"
-            className="shadow-lg"
-          />
-        </div>
-      </section>
+
       <section className=" bg-cloudyGray flex flex-col items-center justify-center p-6 gap-4">
         <h3 className=" font-oswald font-bold text-lg text-center sm:xl md:text-2xl lg:text-3xl">
           WE USE TOP-QUALITY ROOFING MATERIALS TAILORED TO YOUR NEEDS
